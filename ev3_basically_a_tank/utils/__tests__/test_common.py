ccb7aee306da05aa12e48f61dc67ed03ef336a6b
Andy Maskiell <maskiella@gmail.com>
2023-02-21 21:48:57 -0500

    Fix common utils test

--- ev3_basically_a_tank/utils/__tests__/test_common.py
+++ ev3_basically_a_tank/utils/__tests__/test_common.py
@@ -1,3 +1,4 @@
+import io
 import sys
 from unittest.mock import call, patch
 
@@ -5,7 +6,8 @@ from ...utils import debug_logger, safe_init_port
 
 
 def test_debug_logger():
-    with patch("__builtin__.print") as mock_print:
+    # with patch('sys.stdout', new_callable=io.StringIO) as mock_print:
+    with patch("builtins.print") as mock_print:
         debug_logger("can i haz test?", end="\n\n")
         mock_print.assert_has_calls(
             [call("can i haz test?", file=sys.stderr, end="\n\n")]
